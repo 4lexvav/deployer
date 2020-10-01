ee5cdc10493826103918e277de4fbe7320f49330
4lexvav <4lexvav@gmail.com>
2020-10-02 00:06:11 +0300

    deploy new image

--- deployer.go
+++ deployer.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"context"
+	"encoding/base64"
 	"encoding/json"
 	"log"
 	"net/http"
 
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/api/types/network"
 	"github.com/docker/docker/client"
+	"github.com/docker/go-connections/nat"
 )
 
 // State struct used as reply to GET /deploy/ API.
@@ -16,25 +21,99 @@ type State struct {
 }
 
 func deployHandler(w http.ResponseWriter, r *http.Request) {
-	// @todo: auth request by checking Authorization Header
+	// @TODO: auth request by checking Authorization Header
+
+	ctx := context.Background()
+	// cli, err := client.NewEnvClient()
+	cli, err := client.NewClientWithOpts(client.WithAPIVersionNegotiation())
+	if err != nil {
+		panic(err)
+	}
 
 	switch r.Method {
-	case "GET":
-		// handle get method: return info about current image version
-		cli, err := client.NewEnvClient()
-		if err != nil {
-			panic(err)
-		}
-
-		containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	case "GET": // handle get method: return info about current image version
+		containers, err := cli.ContainerList(ctx, types.ContainerListOptions{})
 		if err != nil {
 			panic(err)
 		}
 
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(containers)
-	case "POST":
-		// handle post method: deploy new image' version
+	case "POST": // handle post method: deploy new image' version
+		name := r.FormValue("name")
+		imageName := r.FormValue("image")
+		token := r.FormValue("token")
+		user := r.FormValue("user")
+
+		authJSON, err := json.Marshal(types.AuthConfig{Username: user, Password: token})
+		if err != nil {
+			panic(err)
+		}
+
+		// Pull new image version
+		reader, err := cli.ImagePull(ctx, imageName, types.ImagePullOptions{RegistryAuth: base64.URLEncoding.EncodeToString(authJSON)})
+		if err != nil {
+			panic(err)
+		}
+
+		defer reader.Close()
+
+		args := filters.NewArgs()
+		args.Add("name", name)
+
+		containers, err := cli.ContainerList(ctx, types.ContainerListOptions{Filters: args})
+		if err != nil {
+			panic(err)
+		}
+
+		// Kill old container
+		// err = cli.ContainerKill(ctx, container.ID, "KILL")
+		if len(containers) > 0 {
+			oldContainer := containers[0]
+
+			if err = cli.ContainerStop(ctx, oldContainer.ID, nil); err != nil {
+				panic(err)
+			}
+
+			// Remove old container
+			if err = cli.ContainerRemove(ctx, oldContainer.ID, types.ContainerRemoveOptions{}); err != nil {
+				panic(err)
+			}
+		}
+
+		// docker run -d -t --name test --restart unless-stopped
+		// -e SERVICE_NAME=app -e SERVICE_TAGS=dev
+		// -w /var/www --network jared_app-network --expose 9000
+		// 4lexvav/jared:1.0.0 bash -c "/usr/bin/supervisord -n -c /etc/supervisord.conf"
+
+		// Run new container
+		newContainer, err := cli.ContainerCreate(ctx, &container.Config{
+			Tty:        true,
+			Image:      imageName,
+			WorkingDir: "/var/www",
+			Env:        []string{"SERVICE_NAME=app", "SERVICE_TAGS=dev", "SERVICE_ROLE=web,cron,worker"},
+			Cmd:        []string{"/usr/bin/supervisord", "-n", "-c", "/etc/supervisord.conf"},
+			ExposedPorts: nat.PortSet{
+				"9000/tcp": struct{}{},
+			},
+		}, &container.HostConfig{
+			RestartPolicy: container.RestartPolicy{Name: "unless-stopped"},
+		}, &network.NetworkingConfig{
+			EndpointsConfig: map[string]*network.EndpointSettings{
+				"bridge": {
+					Gateway: "jared_app-network",
+				},
+			},
+		}, name)
+
+		if err != nil {
+			panic(err)
+		}
+
+		cli.ContainerStart(ctx, newContainer.ID, types.ContainerStartOptions{})
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(newContainer)
 	}
 }
 
--- go.mod
+++ go.mod
@@ -3,11 +3,21 @@ module github.com/4lexvav/deployer
 go 1.14
 
 require (
+	github.com/Microsoft/go-winio v0.4.14 // indirect
+	github.com/containerd/containerd v1.4.1 // indirect
 	github.com/docker/distribution v2.7.1+incompatible // indirect
-	github.com/docker/docker v1.13.1
+	github.com/docker/docker v17.12.0-ce-rc1.0.20200916142827-bd33bbf0497b+incompatible
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/docker/go-units v0.4.0 // indirect
+	github.com/gogo/protobuf v1.3.1 // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.0.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	golang.org/x/net v0.0.0-20200813134508-3edf25e44fcc // indirect
+	github.com/sirupsen/logrus v1.6.0 // indirect
+	golang.org/x/net v0.0.0-20200927032502-5d4f70055728 // indirect
+	golang.org/x/sys v0.0.0-20200926100807-9d91bd62050c // indirect
+	google.golang.org/genproto v0.0.0-20200925023002-c2d885f95484 // indirect
+	google.golang.org/grpc v1.32.0 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
 )
